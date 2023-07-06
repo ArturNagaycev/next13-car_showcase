812a116c5b15a339dbe77b40ffbaf047b5474653
Artur.Nahaitsev <artur.nahaitsev@ukad-group.com>
2023-07-06 16:02:05 +0300

    searchbar was implemented

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,7 @@
+// INITIAL VARIANT FROM RAPIDAPI
+
+import { CarProps } from "@/types";
+
 // const url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla";
 // const options = {
 //   method: "GET",
@@ -46,4 +50,18 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
   return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+  const { make, year, model } = car;
+
+  url.searchParams.append("customer", "hrjavascript-mastery");
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  url.searchParams.append("angle", `${angle}`);
+
+  return `${url}`;
 };
